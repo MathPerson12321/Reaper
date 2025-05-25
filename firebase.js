37d355adebe1bb3f5e46119ce9b3e3b83cfd77d5
MathPerson12321 <mathguy12321@gmail.com>
2025-05-25 09:05:05 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,7 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {getDatabase,ref,get,set,push,onValue} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyASmVweFPm03Tizv6J9RMz5nR7THRx7pHU",
-  authDomain: "reaper-clone.firebaseapp.com",
-  databaseURL: "https://reaper-clone-default-rtdb.firebaseio.com",
-  projectId: "reaper-clone",
-  storageBucket: "reaper-clone.firebasestorage.app",
-  messagingSenderId: "329401063543",
-  appId: "1:329401063543:web:4cb1d3ae3668c46a1d4e72",
-  measurementId: "G-EGPV5J832T"
-};
+const firebaseConfig = {}; //You don't get to see this
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
