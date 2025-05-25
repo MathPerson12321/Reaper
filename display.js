442f657c7f356141b34f67e5df858b09ec227c1e
MathPerson12321 <mathguy12321@gmail.com>
2025-05-24 23:28:41 -0600

    Add files via upload

--- display.js
+++ display.js
@@ -1,14 +1,49 @@
+import {readData,writeData,recieveData,ref,db} from "./firebase.js";
+
 let data = null;
-async function getData() {
-    const response = await fetch('data.json');
-    const thisdata = await response.json();
-    data = thisdata;
-}
-async function loadData() {
-    await getData();
+let userlastreaps = null; //Last reap time for a user
+let reaps = null;
+
+document.addEventListener('DOMContentLoaded', async function() {
+    loadUser();
+    const gameinfo = await readData("data");
+    data = gameinfo.val(); 
     console.log(data);
+
+    const lastreaps = await readData("lastuserreap");
+    userlastreaps = lastreaps.val();
+    console.log(userlastreaps);
+
+    const allreaps = await readData("reaps");
+    reaps = allreaps.val();
+    console.log(reaps);
+
+    // Listen for changes to all reaps
+    recieveData("reaps", updatedata);
+});
+
+function updatedata(data){
+    reaps = data.val();
+    console.log("Someone reaped.");
+
+    mostrecentreapdisplay();
 }
-loadData();
+
+
+let userId = null;
+
+function setUser(){
+    userId = crypto.randomUUID();
+    localStorage.setItem('userId', userId);
+}
+
+function loadUser(){
+    userId = localStorage.getItem('userId');
+    console.log(userId);
+    if(!userId){
+        setUser();
+    }
+}
 
 function timetoseconds(milliseconds){
     let seconds = Math.floor(milliseconds/1000);
@@ -27,26 +62,88 @@ function timetoseconds(milliseconds){
     if(minutes > 0){
         return minutes + " minutes, " + seconds + " seconds";
     }
-    if(seconds > 0){
+    else{
         return seconds + " seconds";
     }
 }
 
-function calcTime(){
-    return (data.starttime-unixtime)*1000;
-}
-
-function reaped(){
-    
-}
-
-function getTimeFromUnix(){
-    let time = calcTime();
-    if(data.length > 0){
-        const lastarr = data[data.length-1];
-        const unixtime = Math.floor(Date.now()/1000);
-        time = (unixtime*1000)-lastarr.timereap;
+function getMaxFromReaps(reapnumbers){
+    let maxnum = -Infinity;
+    for (let num of reapnumbers) {
+        if (num > maxnum) {
+            maxnum = num;
+        }
     }
+    return maxnum;
+}
+
+document.getElementById('reapbutton').addEventListener('click', reaped);
+function reaped(){
+    let timereaped = Date.now();
+    writeData("lastuserreap/" + String(userId),timereaped);
+    userlastreaps[String(userId)] = timereaped;
+
+    const reapentry = {
+        user: userId,
+        timestamp: timereaped,
+        timegain: getTimeFromUnix(timereaped)/1000,
+        bonus: 1,
+    };
+
+    //Number the reaps
+    const allkeys = Object.keys(reaps);
+    const reapnumbers = allkeys.map(key=>parseInt(key));
+    let nextnum = null;
+    if (reapnumbers.length > 0) {
+        nextnum = getMaxFromReaps(reapnumbers)+1;
+    } else {
+        nextnum = 1;
+    }
+    writeData("reaps/" + String(nextnum), reapentry);
+    reaps[nextnum] = reapentry;
+
+    console.log(timereaped);
+    console.log(getTimeFromUnix(timereaped)); //Time reaped
+    console.log(reaps);
+}
+
+function getTimeFromUnix(ms){ //Get the reaped time
+    let time = ms-(data.starttime*1000);
+    if(Object.keys(reaps).length>0){
+        const keys = Object.keys(reaps);
+        const keynumbers = keys.map(k=>parseInt(k));
+        const max = getMaxFromReaps(keynumbers);
+        const lastreap = reaps[max];
+        time = ms-lastreap.timestamp;
+    }
+    return time; //Milliseconds
+}
+
+function mostrecentreapdisplay(){
+    const arr = Object.entries(reaps);
+    let count = 1;
+    for(let i=arr.length-1;i>arr.length-11 && i>=0;i--){
+        const [num,details] = arr[i];
+
+        const date = new Date(details.timestamp);
+        const options = {month:"short",day:"2-digit"};
+        const datePart = date.toLocaleDateString("en-US", options);
+        const timePart = date.toTimeString().split(' ')[0]
+        const formatted = datePart + ", " + timePart;
+
+        let str = details.user + " reaped at " + formatted + " and gained " + timetoseconds(details.timegain*1000);
+        document.getElementById(count).innerHTML = str;
+        count++;
+    }
+}
+
+function displayTime(ms){
+    document.getElementById("timer").innerHTML = timetoseconds(getTimeFromUnix(ms));
+}
+
+function displayCooldown(ms){
+    let cooldownleft = (data.cooldown*1000+userlastreaps[String(userId)])-Date.now();
+    document.getElementById("cd").innerHTML = "Cooldown: " + timetoseconds(cooldownleft) + " left";
 }
 
 setInterval(function(){
@@ -58,6 +155,15 @@ setInterval(function(){
         data.gamerunning = true;
         document.getElementById("wait").style.display = "none";
         document.getElementById("game").style.display = "block";
+        displayTime(Date.now());
+        if(Date.now()-userlastreaps[String(userId)] < data.cooldown*1000){
+            document.getElementById("reapstuff").style.display = "none";
+            document.getElementById("cooldown").style.display = "block";
+            displayCooldown(Date.now());
+        }else{
+            document.getElementById("reapstuff").style.display = "block";
+            document.getElementById("cooldown").style.display = "none";
+        }
     }
 },1000);
 
